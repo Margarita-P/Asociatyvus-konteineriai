1e1b45ab0a0821a61623d8891316f2e5c1020d2b
Margarita-P <79039786+Margarita-P@users.noreply.github.com>
2021-06-15 21:13:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,69 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
+void read(vector <string> &url, map <string, pair <set<int>, int>>& words)
+{
+    string text, word;
+    int counter = 0;
+    ifstream in;
+    in.open("duomenys.txt");
+    stringstream inS;
+    inS << in.rdbuf();
+    in.close();
+    
+    while(getline(inS, text))
+    {
+        counter++;
+        while (inS >> word)
+        {
+            if(word.find("http://") != string::npos ||  word.find("www.") != string::npos)
+                url.push_back(word);
+            else
+            {
+                word.erase(remove_if(word.begin(), word.end(), [](char c) {return !isalpha(c);}), word.end());
+                if(!word.empty())
+                {
+                    transform(word.begin(), word.end(), word.begin(), ::tolower);
+                    words[word].first.insert(counter);
+                    words[word].second++;
+                }
+            }
+        }
+    }
+}
+void print(vector <string> url, map <string, pair <set<int>, int>> words)
+{   
+    ofstream out ("rez.txt");
+    for(auto& word : words)
+    {
+        if (word.second.second > 1)
+        {
+            out << "Word: " << word.first << " Count: " << word.second.second << " Lines: ";
+            for(auto line_number : word.second.first)
+                    out << line_number << " ";
+            out << endl;
+        }
+    }
+    out.close();
+
+    cout << "URL's:" << endl;
+    for (int i = 0; i < url.size(); i ++) 
+        cout << url[i] << endl;
+}
 int main()
 {
-
+    vector <string> url;
+    map<string, pair <set<int>, int>>words;    
+    read(url, words);
+    print(url, words);
     return 0;
 }
